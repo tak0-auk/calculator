35a04444d3ba6b8d3555185b7010f6e26649be10
takashima <tak0auk@gmail.com>
2018-02-18 15:30:48 +0900

    temp

--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,8 @@ fn main() {
     let mut input = String::new();
 
     let _ = io::stdin().read_line(&mut input);
-    let chars: Vec<char> = input.trim().chars().collect();
+    let chars: Vec<char> = input.replace(" ", "").trim().chars().collect();
+    let mut token:Vec<String> = Vec::new();
     let mut num: Vec<i64> = Vec::new();
 
     let mut number: String = String::new();
@@ -24,6 +25,7 @@ fn main() {
                 let i = &number.parse::<i64>().unwrap();
                 num.push(i.clone());
                 number.clear();
+                token.push(s.to_string());
                 },
             _ => concat_str(&mut number, s.to_string().as_ref())
         };
@@ -36,12 +38,15 @@ fn main() {
         panic!("not ");
     }
 
-    println!("{}", num.len());
     let mut res = 0;
+    for t in token {
+        println!("{}", t);
+    }
     for i in num {
         res += i;
         println!("i = {} res = {}", i, res)
     }
+    print!("{}", res);
 
 
 
